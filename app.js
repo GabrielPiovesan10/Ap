56bbcce9fb88cf1d6bffc4e1d7d64396f0972e36
GabrielPiovesan10 <gabrielpiovesan3010@gmail.com>
2026-07-23 20:14:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const db = getFirestore(firebaseApp);
 // Coloque aqui os e-mails que terão acesso total (Administradores)
 // Quem não estiver nessa lista, será considerado "Funcionário" e não verá o financeiro.
 const ADMIN_EMAILS = [
-  'gabrielpiovesan3010@gmail.com,alianetp@icloud.com'
+  'gabrielpiovesan3010@gmail.com','alianetp@icloud.com'
 ];
 
 function aplicarPermissoes(userEmail) {
